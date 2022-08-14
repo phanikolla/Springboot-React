72479b7a4cf00d3da9278d6ca1292a8d87dde449
phani <phani.kollas@gmail.com>
2022-08-14 14:46:57 +0530

    This is initial commit for Springboot backend application in the Springboot with react project

---
+++ springboot-backend/src/test/java/net/javaprep/springboot/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaprep.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
